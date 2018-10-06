c3746cf42900666d3e8f63b49ca37717468b5468
Cody <cody@dreamersofday.co>
2018-10-06 16:27:27 -0500

    move pages to pages folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Header } from '../ui';
-import { Home, Login } from '../';
+import { Home, Login } from '../pages';
 
 const App = () => (
   <>
