8145ec55d8fdd6afd435ac783587e0dcbbbd58dd
Cody <cody@dreamersofday.co>
2018-10-06 00:32:41 -0500

    create basic login

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,22 +1,66 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ [event.target.name]: event.target.value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    axios.post('http://bowling-score.test/api/auth/login', this.state)
+      .then(res => {
+        console.log(res);
+      })
+      .catch(error => {
+        console.log('error ', error);
+      })
+
+    console.log('the form was submitted: ' + this.state.email);
+  }
+
   render() {
     return (
       <div className="h-screen flex items-center">
         <div className="container">
           <div className="flex justify-center">
             <div className="w-full md:w-3/4 lg:w-1/2">
-              <div className="bg-white rounded py-8 px-4 shadow-lg">
-                <h1 class="border-b pb-2 mb-4 text-xl">Login</h1>
-                <form onSubmit={this.handleFormSubmit}>
+              <div className="bg-white rounded py-6 px-4 shadow-lg">
+                <h1 className="border-b pb-2 mb-6 text-xl">Login</h1>
+                <form onSubmit={this.handleSubmit}>
                   <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input className="form-control" type="email" id="email" name="email" />
+                    <input
+                      className="form-control"
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.handleChange} />
                   </div>
                   <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input className="form-control" type="password" id="password" name="password" />
+                    <input
+                      className="form-control"
+                      type="password"
+                      id="password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.handleChange} />
+                  </div>
+                  <div className="form-group mb-0">
+                    <button action="submit" className="btn btn--blue">Login</button>
                   </div>
                 </form>
               </div>
