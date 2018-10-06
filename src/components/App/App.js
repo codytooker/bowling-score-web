26d2c269c558b5db8cc2f29bd0ea6988417628c3
Cody <cody@dreamersofday.co>
2018-10-06 16:21:17 -0500

    better naming conventions here

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from '../Home';
-import Login from '../Login';
+import { Home, Login } from '../';
 
 const App = () => (
   <Router>
