03f0258f656ae0534fee67a15d69e42bac125bb5
Cody <cody@dreamersofday.co>
2018-10-07 02:18:00 -0500

    connect signup to store

--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -1,48 +1,65 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
 import { Auth, Card } from '../../UI';
+import * as actions from '../../../actions';
 
-const SignUp = () => (
-  <Auth>
-    <Card title="Sign Up">
-      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-        {({ isSubmitting }) => (
-          <Form>
-            <div className="form-group">
-              <label htmlFor="name">Name</label>
-              <Field className="form-control" type="text" id="name" name="name" />
-              <ErrorMessage name="name">
-                {msg => <span className="form-error">{msg}</span>}
-              </ErrorMessage>
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email</label>
-              <Field className="form-control" type="email" id="email" name="email" />
-              <ErrorMessage name="email">
-                {msg => <span className="form-error">{msg}</span>}
-              </ErrorMessage>
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <Field className="form-control" type="password" id="password" name="password" />
-              <ErrorMessage name="password">
-                {msg => <span className="form-error">{msg}</span>}
-              </ErrorMessage>
-            </div>
-            <div className="form-group mb-0">
-              <button type="submit" className="btn btn--blue" disabled={isSubmitting}>Sign Up</button>
-            </div>
-          </Form>
-        )}
-      </Formik>
-    </Card>
-    <p className="text-center py-6">Already a member <Link to="/login">Click Here</Link> to Login</p>
-  </Auth>
-);
+class SignUp extends Component {
+  onSubmit = (values, actions) => {
+    this.props.signup(values)
+      .then(res => {
+        actions.setSubmitting(false);
+        console.log(res);
+      })
+      .catch(error => {
+        actions.setSubmitting(false);
+        actions.setErrors(error.response.data.errors);
+      })
+  }
+
+  render() {
+    return (
+      <Auth>
+        <Card title="Sign Up">
+          <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={this.onSubmit}>
+            {({ isSubmitting }) => (
+              <Form>
+                <div className="form-group">
+                  <label htmlFor="name">Name</label>
+                  <Field className="form-control" type="text" id="name" name="name" />
+                  <ErrorMessage name="name">
+                    {msg => <span className="form-error">{msg}</span>}
+                  </ErrorMessage>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="email">Email</label>
+                  <Field className="form-control" type="email" id="email" name="email" />
+                  <ErrorMessage name="email">
+                    {msg => <span className="form-error">{msg}</span>}
+                  </ErrorMessage>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <Field className="form-control" type="password" id="password" name="password" />
+                  <ErrorMessage name="password">
+                    {msg => <span className="form-error">{msg}</span>}
+                  </ErrorMessage>
+                </div>
+                <div className="form-group mb-0">
+                  <button type="submit" className="btn btn--blue" disabled={isSubmitting}>Sign Up</button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </Card>
+        <p className="text-center py-6">Already a member <Link to="/login">Click Here</Link> to Login</p>
+      </Auth>
+    );
+  }
+};
 
 const initialValues = {
   name: '',
@@ -58,16 +75,4 @@ const validationSchema = (
   })
 );
 
-const handleSubmit = (values, actions) => {
-  axios.post('http://bowling-score.test/api/auth/register', values)
-    .then(res => {
-      actions.setSubmitting(false);
-      console.log(res);
-    })
-    .catch(error => {
-      actions.setSubmitting(false);
-      actions.setErrors(error.response.data.errors);
-    })
-}
-
-export default SignUp;
+export default connect(null, actions)(SignUp);
