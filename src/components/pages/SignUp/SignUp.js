3e7b5822f53327831e62a46d876cbf7b9cbe4dfe
Cody <cody@dreamersofday.co>
2018-10-06 20:19:39 -0500

    use the Auth component in the signup page

--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -4,50 +4,44 @@ import { Link } from 'react-router-dom';
 import * as Yup from 'yup';
 import axios from 'axios';
 
-import { Card } from '../../UI';
+import { Auth, Card } from '../../UI';
 
 const SignUp = () => (
-  <div className="h-screen flex items-center">
-    <div className="container">
-      <div className="flex justify-center">
-        <div className="w-full md:w-3/4 lg:w-1/2">
-          <Card title="Sign Up">
-            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
-              {({ isSubmitting }) => (
-                <Form>
-                  <div className="form-group">
-                    <label htmlFor="name">Name</label>
-                    <Field className="form-control" type="text" id="name" name="name" />
-                    <ErrorMessage name="name">
-                      {msg => <span className="form-error">{msg}</span>}
-                    </ErrorMessage>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <Field className="form-control" type="email" id="email" name="email" />
-                    <ErrorMessage name="email">
-                      {msg => <span className="form-error">{msg}</span>}
-                    </ErrorMessage>
-                  </div>
-                  <div className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <Field className="form-control" type="password" id="password" name="password" />
-                    <ErrorMessage name="password">
-                      {msg => <span className="form-error">{msg}</span>}
-                    </ErrorMessage>
-                  </div>
-                  <div className="form-group mb-0">
-                    <button type="submit" className="btn btn--blue" disabled={isSubmitting}>Sign Up</button>
-                  </div>
-                </Form>
-              )}
-            </Formik>
-          </Card>
-          <p className="text-center py-6">Already a member <Link to="/login">Click Here</Link> to Login</p>
-        </div>
-      </div>
-    </div>
-  </div>
+  <Auth>
+    <Card title="Sign Up">
+      <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
+        {({ isSubmitting }) => (
+          <Form>
+            <div className="form-group">
+              <label htmlFor="name">Name</label>
+              <Field className="form-control" type="text" id="name" name="name" />
+              <ErrorMessage name="name">
+                {msg => <span className="form-error">{msg}</span>}
+              </ErrorMessage>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <Field className="form-control" type="email" id="email" name="email" />
+              <ErrorMessage name="email">
+                {msg => <span className="form-error">{msg}</span>}
+              </ErrorMessage>
+            </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <Field className="form-control" type="password" id="password" name="password" />
+              <ErrorMessage name="password">
+                {msg => <span className="form-error">{msg}</span>}
+              </ErrorMessage>
+            </div>
+            <div className="form-group mb-0">
+              <button type="submit" className="btn btn--blue" disabled={isSubmitting}>Sign Up</button>
+            </div>
+          </Form>
+        )}
+      </Formik>
+    </Card>
+    <p className="text-center py-6">Already a member <Link to="/login">Click Here</Link> to Login</p>
+  </Auth>
 );
 
 const initialValues = {
