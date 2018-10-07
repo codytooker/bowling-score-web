14b984a40b71402a54da7a605dff42602da488ae
Cody <cody@dreamersofday.co>
2018-10-06 19:39:34 -0500

    convert signup form to use Formik

--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -1,77 +1,93 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { Link } from 'react-router-dom';
+import * as Yup from 'yup';
 import axios from 'axios';
 
-class Login extends Component {
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      password: '',
-    }
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+};
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    axios.post('http://bowling-score.test/api/auth/login', this.state)
-      .then(res => {
-        console.log(res);
-      })
-      .catch(error => {
-        console.log('error ', error);
-      })
-
-    console.log('the form was submitted: ' + this.state.email);
-  }
-
-  render() {
-    return (
-      <div className="h-screen flex items-center">
-        <div className="container">
-          <div className="flex justify-center">
-            <div className="w-full md:w-3/4 lg:w-1/2">
-              <div className="bg-white rounded py-6 px-4 shadow-lg">
-                <h1 className="border-b pb-2 mb-6 text-xl">Sign Up</h1>
-                <form onSubmit={this.handleSubmit}>
+const SignUp = () => (
+  <div className="h-screen flex items-center">
+    <div className="container">
+      <div className="flex justify-center">
+        <div className="w-full md:w-3/4 lg:w-1/2">
+          <div className="bg-white rounded py-6 px-4 shadow-lg">
+            <h1 className="border-b pb-2 mb-6 text-xl">Sign Up</h1>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={Yup.object({
+                name: Yup.string().required('Required'),
+                email: Yup.string().email('Invalid email!').required('Required'),
+                password: Yup.string().required('Required'),
+              })}
+              onSubmit={(values, actions) => {
+                axios.post('http://bowling-score.test/api/auth/register', values)
+                  .then(res => {
+                    actions.setSubmitting(false);
+                    console.log(res);
+                  })
+                  .catch(error => {
+                    console.log('we are in the error section');
+                    actions.setSubmitting(false);
+                    actions.setErrors(error.response.data.errors);
+                    console.log('error ', error.response);
+                  })
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form>
+                  <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <Field className="form-control" type="text" id="name" name="name" />
+                    <ErrorMessage name="name">
+                      {msg => <span className="form-error">{msg}</span>}
+                    </ErrorMessage>
+                  </div>
                   <div className="form-group">
                     <label htmlFor="email">Email</label>
-                    <input
-                      className="form-control"
-                      type="email"
-                      id="email"
-                      name="email"
-                      value={this.state.email}
-                      onChange={this.handleChange} />
+                    <Field className="form-control" type="email" id="email" name="email" />
+                    <ErrorMessage name="email">
+                      {msg => <span className="form-error">{msg}</span>}
+                    </ErrorMessage>
                   </div>
                   <div className="form-group">
                     <label htmlFor="password">Password</label>
-                    <input
-                      className="form-control"
-                      type="password"
-                      id="password"
-                      name="password"
-                      value={this.state.password}
-                      onChange={this.handleChange} />
+                    <Field className="form-control" type="password" id="password" name="password" />
+                    <ErrorMessage name="password">
+                      {msg => <span className="form-error">{msg}</span>}
+                    </ErrorMessage>
                   </div>
                   <div className="form-group mb-0">
-                    <button action="submit" className="btn btn--blue">Login</button>
+                    <button type="submit" className="btn btn--blue" disabled={isSubmitting}>Sign Up</button>
                   </div>
-                </form>
-              </div>
-              <p className="text-center py-6">Already a member <Link to="/login">Click Here</Link> to Login</p>
-            </div>
+                </Form>
+              )}
+            </Formik>
           </div>
+          <p className="text-center py-6">Already a member <Link to="/login">Click Here</Link> to Login</p>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
 
-export default Login;
+export default SignUp;
+
+
+
+// handleSubmit(event) {
+//   event.preventDefault();
+//   axios.post('http://bowling-score.test/api/auth/register', this.state)
+//     .then(res => {
+//       console.log(res);
+//     })
+//     .catch(error => {
+//       console.log('error ', error);
+//     })
+
+//   console.log('the form was submitted: ' + this.state.email);
+// }
