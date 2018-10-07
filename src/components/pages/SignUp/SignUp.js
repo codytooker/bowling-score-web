a6d58cc9ef3467348e73cb9b84b5f28ce2c3ddbf
Cody <cody@dreamersofday.co>
2018-10-06 20:00:11 -0500

    clean up the formik props so its easier to read the component

--- src/components/pages/SignUp/SignUp.js
+++ src/components/pages/SignUp/SignUp.js
@@ -6,39 +6,13 @@ import axios from 'axios';
 
 import { Card } from '../../UI';
 
-const initialValues = {
-  name: '',
-  email: '',
-  password: '',
-};
-
 const SignUp = () => (
   <div className="h-screen flex items-center">
     <div className="container">
       <div className="flex justify-center">
         <div className="w-full md:w-3/4 lg:w-1/2">
           <Card title="Sign Up">
-            <Formik
-              initialValues={initialValues}
-              validationSchema={Yup.object({
-                name: Yup.string().required('Required'),
-                email: Yup.string().email('Invalid email!').required('Required'),
-                password: Yup.string().required('Required'),
-              })}
-              onSubmit={(values, actions) => {
-                axios.post('http://bowling-score.test/api/auth/register', values)
-                  .then(res => {
-                    actions.setSubmitting(false);
-                    console.log(res);
-                  })
-                  .catch(error => {
-                    console.log('we are in the error section');
-                    actions.setSubmitting(false);
-                    actions.setErrors(error.response.data.errors);
-                    console.log('error ', error.response);
-                  })
-              }}
-            >
+            <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={handleSubmit}>
               {({ isSubmitting }) => (
                 <Form>
                   <div className="form-group">
@@ -76,19 +50,30 @@ const SignUp = () => (
   </div>
 );
 
+const initialValues = {
+  name: '',
+  email: '',
+  password: '',
+};
+
+const validationSchema = (
+  Yup.object({
+    name: Yup.string().required('Required'),
+    email: Yup.string().email('Invalid email!').required('Required'),
+    password: Yup.string().required('Required'),
+  })
+);
+
+const handleSubmit = (values, actions) => {
+  axios.post('http://bowling-score.test/api/auth/register', values)
+    .then(res => {
+      actions.setSubmitting(false);
+      console.log(res);
+    })
+    .catch(error => {
+      actions.setSubmitting(false);
+      actions.setErrors(error.response.data.errors);
+    })
+}
+
 export default SignUp;
-
-
-
-// handleSubmit(event) {
-//   event.preventDefault();
-//   axios.post('http://bowling-score.test/api/auth/register', this.state)
-//     .then(res => {
-//       console.log(res);
-//     })
-//     .catch(error => {
-//       console.log('error ', error);
-//     })
-
-//   console.log('the form was submitted: ' + this.state.email);
-// }
