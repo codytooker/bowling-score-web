b553143d2c97b441e46775f549e79c30617eaf2a
Cody <cody@dreamersofday.co>
2018-10-07 02:17:26 -0500

    create an auth user action

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+import {
+  AUTH_USER
+} from './types';
+
+export const signup = values => dispatch => {
+  return axios.post('http://bowling-score.test/api/auth/register', values)
+    .then(res => {
+      dispatch({ type: AUTH_USER, payload: res.data.access_token });
+    });
+};
