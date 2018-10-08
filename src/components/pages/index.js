a71b8e215e01a184512c0b7312e947bc46a6609d
Cody <cody@dreamersofday.co>
2018-10-07 23:32:39 -0500

    add signout route

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,4 +1,5 @@
 export { default as Home } from './Home/Home';
 export { default as Login } from './Login/Login';
 export { default as SignUp } from './SignUp/SignUp';
+export { default as SignOut } from './SignOut/SignOut';
 export { default as Feature } from './Feature/Feature';
