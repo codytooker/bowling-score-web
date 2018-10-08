6d46c56568d09c4c8c950d33403cb812b68451e1
Cody <cody@dreamersofday.co>
2018-10-08 11:01:27 -0500

    create logged out home view

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Home = () => (
-  <div>Home</div>
-);
+import { isAuthenticated } from '../../../reducers/auth';
 
-export default Home;
+class Home extends Component {
+  renderLinks() {
+    if (this.props.isAuthenticated) {
+      return (
+        <div>
+          <Link to="/feature">feature</Link>
+          <Link to="/signout">Sign Out</Link>
+        </div>
+      )
+    } else {
+      return (
+        <div className="flex flex-col w-3/4 mx-auto">
+          <Link class="btn btn--white btn--lg mb-12" to="/login">login</Link>
+          <Link class="btn btn--white btn--lg" to="/signup">signup</Link>
+        </div>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <div className="h-screen flex items-center">
+        <div className="container text-center">
+          <h1 class="mb-12 text-white">Bowling Scores</h1>
+          <nav>
+            {this.renderLinks()}
+          </nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: isAuthenticated(state),
+  };
+};
+
+export default connect(mapStateToProps)(Home);
