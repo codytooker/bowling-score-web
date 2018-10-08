7a3e58fb06d6758803d401506d8eccf9786c189a
Cody <cody@dreamersofday.co>
2018-10-07 23:55:37 -0500

    show correct links in the header

--- src/components/UI/Header.js
+++ src/components/UI/Header.js
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-const Header = () => (
-  <nav>
-    <Link to="/">home</Link>
-    <Link to="/login">login</Link>
-    <Link to="/signup">signup</Link>
-    <Link to="/feature">feature</Link>
-    <Link to="/signout">Sign Out</Link>
-  </nav>
-);
+import { isAuthenticated } from '../../reducers/auth';
 
-export default Header;
+class Header extends Component {
+  renderLinks() {
+    if (this.props.isAuthenticated) {
+      return (
+        <>
+          <Link to="/feature">feature</Link>
+          <Link to="/signout">Sign Out</Link>
+        </>
+      )
+    } else {
+      return (
+        <>
+          <Link to="/login">login</Link>
+          <Link to="/signup">signup</Link>
+        </>
+      )
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <Link to="/">home</Link>
+        {this.renderLinks()}
+      </nav>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: isAuthenticated(state),
+  };
+};
+
+export default connect(mapStateToProps)(Header);
