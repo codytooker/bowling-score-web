8f36ed6305e0038bb5adf8789b018e0c29adf5a6
Cody <cody@dreamersofday.co>
2018-10-08 11:01:07 -0500

    remove header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,20 +3,16 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Home, Login, SignUp, SignOut, Feature } from '../pages';
 import { PrivateRoute, GuestRoute } from '../utils';
-import Header from '../UI/Header';
 
 const App = () => (
   <Router>
-    <>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <GuestRoute exact path="/login" component={Login} />
-        <GuestRoute exact path="/SignUp" component={SignUp} />
-        <PrivateRoute exact path="/feature" component={Feature} />
-        <PrivateRoute exact path="/signout" component={SignOut} />
-      </Switch>
-    </>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <GuestRoute exact path="/login" component={Login} />
+      <GuestRoute exact path="/SignUp" component={SignUp} />
+      <PrivateRoute exact path="/feature" component={Feature} />
+      <PrivateRoute exact path="/signout" component={SignOut} />
+    </Switch>
   </Router>
 );
 
