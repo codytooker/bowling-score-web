2d4cae698338a204318463712188679476036231
Cody <cody@dreamersofday.co>
2018-10-16 08:54:09 -0500

    add some base game actions

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,4 +1,61 @@
 import axios from 'axios';
+import { normalize } from 'normalizr';
+
+import * as schema from './schema';
+
+import {
+  REQUEST_GAMES,
+  RECEIVE_GAMES,
+  INVALIDATE_GAMES,
+} from './types';
+
+export const invalidateGames = () => {
+  return {
+    type: INVALIDATE_GAMES
+  }
+}
+
+const requestGames = () => {
+  return {
+    type: REQUEST_GAMES,
+  }
+}
+
+const receiveGames = data => {
+  return {
+    type: RECEIVE_GAMES,
+    payload: {
+      receivedAt: Date.now(),
+      games: data.entities.games,
+      gameIds: data.result,
+    }
+  }
+}
+
+const shouldFetchGames = ({ games }) => {
+  if (!games.allIds.length) {
+    return true;
+  } else if(games.isFetching) {
+    return false;
+  } else {
+    return games.didInvalidate;
+  }
+}
+
+const fetchGames = () => dispatch => {
+  dispatch(requestGames());
+  return axios.get('/games')
+    .then(res => {
+      const normalizedData = normalize(res.data.games, [schema.game]);
+      dispatch(receiveGames(normalizedData));
+    });
+}
+
+export const fetchGamesIfNeeded = () => (dispatch, getState) => {
+  if (shouldFetchGames(getState())) {
+    return dispatch(fetchGames());
+  }
+}
 
 export const createGame = values => dispatch => {
   console.log(values);
@@ -14,3 +71,4 @@ export const createGame = values => dispatch => {
       console.log('this is temporary just seeing if it we get to here, which we should')
     });
 }
+
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,5 @@
 export const AUTH_USER = 'AUTH_USER';
+
+export const REQUEST_GAMES = 'REQUEST_GAMES';
+export const RECEIVE_GAMES = 'RECEIVE_GAMES';
+export const INVALIDATE_GAMES = 'INVALIDATE_GAMES';
