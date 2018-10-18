495d1764c0b59a3062b3477936cd9847e15e795b
Cody <cody@dreamersofday.co>
2018-10-18 16:28:24 -0500

    clean up single game

--- src/components/pages/SingleGame/SingleGame.js
+++ src/components/pages/SingleGame/SingleGame.js
@@ -3,38 +3,21 @@ import { connect } from 'react-redux';
 
 import { DefaultLayout } from '../../UI/Layouts';
 import { Heading } from '../../UI/elements';
-import { fetchGamesIfNeeded } from '../../../actions/game';
 import { getGameByID } from '../../../reducers/games';
 
 class SingleGame extends Component {
-  componentDidMount() {
-    this.props.fetchGamesIfNeeded();
-  }
-
-  renderGames() {
-    if (this.props.isLoading) {
-      return <div>Show Loading</div>;
-    }
-    if (this.props.games.length) {
-      return (
-          <div>
-            {this.props.games.map(game => <div key={game.id}>{game.title}</div>)}
-          </div>
-      );
-    }
-  }
-
   render() {
+    const { game } = this.props;
+
     return (
       <DefaultLayout>
-        <Heading>My Games</Heading>
-        {this.renderGames()}
+        <Heading>{game.title}</Heading>
       </DefaultLayout>
     );
   }
 }
 
 const mapStateToProps = (state, ownProps) => ({
-  games: getGameByID(state, ownProps.match.params.id),
+  game: getGameByID(state, ownProps.match.params.id),
 });
-export default connect(mapStateToProps, { fetchGamesIfNeeded })(SingleGame);
+export default connect(mapStateToProps)(SingleGame);
