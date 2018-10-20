43380986a84c5f9dc4a83414f02c4b0569574a6b
Cody <cody@dreamersofday.co>
2018-10-19 23:15:33 -0500

    export frame controls component

--- src/components/UI/bowling/index.js
+++ src/components/UI/bowling/index.js
@@ -1,2 +1,3 @@
 export { default as PinCounter } from './PinCounter/PinCounter';
 export { default as FullBoard } from './FullBoard/FullBoard';
+export { default as FrameControls } from './FrameControls/FrameControls';
