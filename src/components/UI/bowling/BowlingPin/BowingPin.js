74de361b8c4d60c4a89d2f4c95134ccf2659f628
Cody <cody@dreamersofday.co>
2018-10-20 16:03:05 -0500

    remove arrow function in bowling pin click

--- src/components/UI/bowling/BowlingPin/BowingPin.js
+++ src/components/UI/bowling/BowlingPin/BowingPin.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cs from 'classnames';
 
-const BowlingPin = ({ number, selected, handleClick }) => {
-  const pinClass = cs(
-    'block w-12 h-12 m-2 border rounded-full flex items-center justify-center focus:outline-none focus:shadow-outline',
-    { 'bg-blue border-blue text-white': selected },
-  );
-  return (
-    <button onClick={() => handleClick(number)} className={pinClass}>{number}</button>
-  );
-};
+class BowlingPin extends Component {
+  handleClick = () => {
+    this.props.handleClick(this.props.number);
+  }
+
+  render() {
+    const { number, selected } = this.props;
+    const pinClass = cs(
+      'block w-12 h-12 m-2 border rounded-full flex items-center justify-center focus:outline-none focus:shadow-outline',
+      { 'bg-blue border-blue text-white': selected },
+    );
+
+    return (
+      <button onClick={this.handleClick} className={pinClass}>{number}</button>
+    );
+  }
+}
 
 
 BowlingPin.defaultProps = {
