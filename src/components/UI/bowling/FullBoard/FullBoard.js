d83b1fe7b98924498e5954c95a7ad6783d57b5a5
Cody <cody@dreamersofday.co>
2018-10-19 23:13:09 -0500

    add full board componetn

--- src/components/UI/bowling/FullBoard/FullBoard.js
+++ src/components/UI/bowling/FullBoard/FullBoard.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const FullBoard = () => (
-    <div>Testin</div>
-);
+import Frame from '../Frame/Frame';
+
+const FullBoard = () => {
+  const frames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <div className="flex items-stretch select-none">
+      {
+        frames.map(frame => (
+          <Frame frame={frame} key={frame} />
+        ))
+      }
+    </div>
+  );
+};
 
 export default FullBoard;
