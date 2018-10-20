bd4b5a4353bd540d5d40cecc8e0f783436c2b64e
Cody <cody@dreamersofday.co>
2018-10-19 23:27:16 -0500

    create default frame util for handling frames

---
+++ src/utils/defaultFrame.js
@@ -0,0 +1,12 @@
+export default [
+  { id: 1, status: 'open' },
+  { id: 2, status: 'open' },
+  { id: 3, status: 'open' },
+  { id: 4, status: 'open' },
+  { id: 5, status: 'open' },
+  { id: 6, status: 'open' },
+  { id: 7, status: 'open' },
+  { id: 8, status: 'open' },
+  { id: 9, status: 'open' },
+  { id: 10, status: 'open' },
+];
