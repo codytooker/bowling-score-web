21a9e42f0bf4b2b087e7220914627a85b7e74a0b
Cody <cody@dreamersofday.co>
2018-10-20 00:07:47 -0500

    add updateFrame Prop to frame controls

--- src/components/UI/bowling/FrameControls/FrameControls.js
+++ src/components/UI/bowling/FrameControls/FrameControls.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const FrameControlers = () => (
+const FrameControls = ({ updateFrame }) => (
   <div className="py-6 px-2 flex justify-around">
-    <button className="btn btn--white" type="button">Prev</button>
+    <button onClick={() => updateFrame(false)} className="btn btn--white" type="button">Prev</button>
     <button className="btn btn--white" type="button">Strike</button>
     <button className="btn btn--white" type="button">Spare</button>
-    <button className="btn btn--white" type="button">Next</button>
+    <button onClick={() => updateFrame()} className="btn btn--white" type="button">Next</button>
   </div>
 );
 
-export default FrameControlers;
+FrameControls.propTypes = {
+  updateFrame: PropTypes.func.isRequired,
+};
+
+export default FrameControls;
+
+// TODO: Next should move you to next ball regardless of frame
+// TODO: Next shouldn't be able to go to the next frame if the previous one isn't finished
+// TODO: Strike should only work when first ball is active
+// TODO: Spare should only work when second ball is active
