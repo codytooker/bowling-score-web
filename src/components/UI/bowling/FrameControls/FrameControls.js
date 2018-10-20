1afe54e2d784e9746f657b49d8c5b52211a6baa7
Cody <cody@dreamersofday.co>
2018-10-19 23:42:14 -0500

    update class to classname in frame controls

--- src/components/UI/bowling/FrameControls/FrameControls.js
+++ src/components/UI/bowling/FrameControls/FrameControls.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const FrameControlers = () => (
-  <div class="py-6 px-2 flex justify-around">
-    <button class="btn btn--white" type="button">Prev</button>
-    <button class="btn btn--white" type="button">Strike</button>
-    <button class="btn btn--white" type="button">Spare</button>
-    <button class="btn btn--white" type="button">Next</button>
+  <div className="py-6 px-2 flex justify-around">
+    <button className="btn btn--white" type="button">Prev</button>
+    <button className="btn btn--white" type="button">Strike</button>
+    <button className="btn btn--white" type="button">Spare</button>
+    <button className="btn btn--white" type="button">Next</button>
   </div>
 );
 
