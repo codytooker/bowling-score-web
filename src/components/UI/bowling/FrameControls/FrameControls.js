cdbe9cfb028e5951c43783f4f213518cb31c7362
Cody <cody@dreamersofday.co>
2018-10-19 23:12:59 -0500

    add Frame Controls component

---
+++ src/components/UI/bowling/FrameControls/FrameControls.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FrameControlers = () => (
+  <div class="py-6 px-2 flex justify-around">
+    <button class="btn btn--white" type="button">Prev</button>
+    <button class="btn btn--white" type="button">Strike</button>
+    <button class="btn btn--white" type="button">Spare</button>
+    <button class="btn btn--white" type="button">Next</button>
+  </div>
+);
+
+export default FrameControlers;
