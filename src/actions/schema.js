bce5bee87b4737a9c00d3f6181af94dfd78548f3
Cody <cody@dreamersofday.co>
2018-10-21 12:59:19 -0500

    add some default values to schemas

--- src/actions/schema.js
+++ src/actions/schema.js
@@ -1,6 +1,15 @@
 import { schema } from 'normalizr';
 
-const frame = new schema.Entity('frames');
-export const game = new schema.Entity('games', {
-  frames: [frame],
+const frame = new schema.Entity('frames', {}, {
+  processStrategy: value => ({
+    ...value,
+    score: null,
+  }),
+});
+
+export const game = new schema.Entity('games', { frames: [frame] }, {
+  processStrategy: value => ({
+    ...value,
+    score: 0,
+  }),
 });
