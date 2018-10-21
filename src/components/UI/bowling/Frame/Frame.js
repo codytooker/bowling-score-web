8fd64562cd2e6babf6f7530feecbeee88b66b71d
Cody <cody@dreamersofday.co>
2018-10-21 18:39:59 -0500

    pass real frames from redux to frame.js

--- src/components/UI/bowling/Frame/Frame.js
+++ src/components/UI/bowling/Frame/Frame.js
@@ -9,20 +9,27 @@ const Frame = ({ frame, active }) => {
   );
 
   return (
-    <div key={frame} className={frameClass}>
-      <div className="border-b border-black text-center">{frame}</div>
+    <div className={frameClass}>
+      <div className="border-b border-black text-center">{frame.number}</div>
       <div className="flex border-b border-black">
-        <span className="flex-grow border-r border-black text-center">5</span>
-        <span className="flex-grow text-center">5</span>
+        <span className="flex-grow border-r border-black text-center">{frame.throw_1.length}</span>
+        <span className="flex-grow text-center">{frame.throw_2.length}</span>
       </div>
-      <div className="border-b border-black text-center">10</div>
+      <div className="h-6 border-b border-black text-center">{frame.score}</div>
       <div>pins</div>
     </div>
   );
 };
 
 Frame.propTypes = {
-  frame: PropTypes.number.isRequired,
+  frame: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    number: PropTypes.number.isRequired,
+    throw_1: PropTypes.array,
+    throw_2: PropTypes.array,
+    throw_3: PropTypes.array,
+    score: PropTypes.number,
+  }).isRequired,
   active: PropTypes.bool.isRequired,
 };
 
--- src/components/UI/bowling/FullBoard/FullBoard.js
+++ src/components/UI/bowling/FullBoard/FullBoard.js
@@ -3,28 +3,24 @@ import PropTypes from 'prop-types';
 
 import Frame from '../Frame/Frame';
 
-const FullBoard = ({ currentFrame }) => {
-  const frames = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-  return (
+const FullBoard = ({ currentFrame, frames }) => (
     <div className="flex items-stretch select-none">
       {
         frames.map(frame => (
-          <Frame frame={frame} key={frame} active={frame === currentFrame} />
+          <Frame frame={frame} key={frame.id} active={frame === currentFrame} />
         ))
       }
     </div>
-  );
-};
+);
 
 FullBoard.propTypes = {
   currentFrame: PropTypes.number.isRequired,
   frames: PropTypes.arrayOf(PropTypes.shape({
     id: PropTypes.number.isRequired,
     number: PropTypes.number.isRequired,
+    throw_1: PropTypes.array,
     throw_2: PropTypes.array,
     throw_3: PropTypes.array,
-    throw_1: PropTypes.array,
     score: PropTypes.number,
   })).isRequired,
 };
