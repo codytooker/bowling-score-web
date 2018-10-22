27f909d28c487173e642f71c471b28f135e5290d
Cody <cody@dreamersofday.co>
2018-10-22 00:47:12 -0500

    pass in correct throws to Frame component

--- src/components/UI/bowling/Frame/Frame.js
+++ src/components/UI/bowling/Frame/Frame.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import cs from 'classnames';
 
-const Frame = ({ frame, active }) => {
-  const frameClass = cs(
-    'flex-grow border border-black',
-    { 'bg-white': active },
-  );
+class Frame extends Component {
+  getBallScore = ball => (ball !== null ? ball.length : '');
 
-  return (
-    <div className={frameClass}>
-      <div className="border-b border-black text-center">{frame.number}</div>
-      <div className="flex border-b border-black">
-        <span className="flex-grow border-r border-black text-center">{frame.throw_1.length}</span>
-        <span className="flex-grow text-center">{frame.throw_2.length}</span>
-        {frame.number === 10 && <span className="flex-grow border-l border-black text-center">{frame.throw_3 !== null ? frame.throw_3 : null}</span>}
+  render() {
+    const { frame, active } = this.props;
+
+    const frameClass = cs(
+      'flex-grow border border-black',
+      { 'bg-white': active },
+    );
+
+    return (
+      <div className={frameClass}>
+        <div className="border-b border-black text-center">{frame.number}</div>
+        <div className="h-6 flex border-b border-black">
+          <span className="flex-grow border-r border-black text-center">{this.getBallScore(frame.throw_1)}</span>
+          <span className="flex-grow text-center">{this.getBallScore(frame.throw_2)}</span>
+          {frame.number === 10 && <span className="flex-grow border-l border-black text-center">{this.getBallScore(frame.throw_2)}</span>}
+        </div>
+        <div className="h-6 border-b border-black text-center">{frame.score}</div>
+        <div>pins</div>
       </div>
-      <div className="h-6 border-b border-black text-center">{frame.score}</div>
-      <div>pins</div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 Frame.propTypes = {
   frame: PropTypes.shape({
