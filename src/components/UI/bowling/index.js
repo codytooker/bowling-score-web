04a56b809495e2b4921ee3af9f96176d9249514a
Cody <cody@dreamersofday.co>
2018-10-23 00:30:38 -0500

    remove frame controls export

--- src/components/UI/bowling/index.js
+++ src/components/UI/bowling/index.js
@@ -1,3 +1,2 @@
 export { default as PinCounter } from './PinCounter/PinCounter';
 export { default as FullBoard } from './FullBoard/FullBoard';
-export { default as FrameControls } from './FrameControls/FrameControls';
