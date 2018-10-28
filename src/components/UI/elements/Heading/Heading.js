6b3728cba3434539114329bf19a47e82a4e5e529
Cody <cody@dreamersofday.co>
2018-10-28 18:46:38 -0500

    Lint Heading

--- src/components/UI/elements/Heading/Heading.js
+++ src/components/UI/elements/Heading/Heading.js
@@ -5,17 +5,17 @@ const Heading = ({ children, level, className, ...rest }) => {
   const HeadingLevel = `h${level}`;
   const classNames = ['text-center text-white', className].join(' ');
 
-  return <HeadingLevel {...rest} className={classNames}>{children}</HeadingLevel>
-}
+  return <HeadingLevel {...rest} className={classNames}>{children}</HeadingLevel>;
+};
 
 Heading.defaultProps = {
   level: '1',
-}
+};
 
 Heading.propTypes = {
   children: PropTypes.string.isRequired,
-  level: PropTypes.oneOf(['1', '2', '3', '4', '5', '6',]).isRequired,
+  level: PropTypes.oneOf(['1', '2', '3', '4', '5', '6']).isRequired,
   className: PropTypes.string,
-}
+};
 
 export default Heading;
