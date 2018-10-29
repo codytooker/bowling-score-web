251e9802499ce51d1c7c7802ee95b5be2d25b5cc
Cody <cody@dreamersofday.co>
2018-10-28 23:20:57 -0500

    add bottom margin on single game heading

--- src/components/pages/SingleGame/SingleGame.js
+++ src/components/pages/SingleGame/SingleGame.js
@@ -273,7 +273,7 @@ class SingleGame extends Component {
 
     return (
       <DefaultLayout>
-        <Heading>{game.title}</Heading>
+        <Heading className="mb-4">{game.title}</Heading>
         <FullBoard
           currentFrame={currentFrame}
           currentBall={currentBall}
