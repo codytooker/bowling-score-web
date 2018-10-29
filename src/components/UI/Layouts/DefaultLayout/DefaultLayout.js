32c5671162ed423ad36ebc220071838e06ee72d7
Cody <cody@dreamersofday.co>
2018-10-29 00:15:11 -0500

    add some space on bottom bar links

--- src/components/UI/Layouts/DefaultLayout/DefaultLayout.js
+++ src/components/UI/Layouts/DefaultLayout/DefaultLayout.js
@@ -8,7 +8,7 @@ const DefaultLayout = ({ children }) => (
         {children}
       </div>
     </main>
-    <footer className="fixed pin-b pin-l pin-r bg-black py-4 text-center">
+    <footer className="fixed pin-b pin-l pin-r bg-black py-4 px-8 flex justify-between text-center">
       <Link className="font-bold text-sm no-underline uppercase tracking-wide mx-2" to="/">Home</Link>
       <Link className="font-bold text-sm no-underline uppercase tracking-wide mx-2" to="/games">My Games</Link>
       <Link className="font-bold text-sm no-underline uppercase tracking-wide mx-2" to="/new-game">New Game</Link>
