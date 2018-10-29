058cd9ef0a11ab7c0f04dd35cba8358913d701a1
Cody <cody@dreamersofday.co>
2018-10-29 14:30:18 -0500

    prepping for pugecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,27 @@
 /* eslint-disable */
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || [];
+  }
+}
+
 module.exports = {
   plugins: [
     require('postcss-import')(),
     require('tailwindcss')('./tailwind.js'),
     require('postcss-nested')(),
     require('autoprefixer')(),
+    process.env.NODE_ENV === "production" &&  purgecss({
+      content: ['./src/**/*.js', './public/**/*.html'],
+      css: ['./src/styles/index.css'],
+      extractors: [
+        {
+          extractor: TailwindExtractor,
+          extensions: ['js', 'html']
+        }
+      ],
+    }),
   ],
 };
