ceb7135710d8991a59a835809e6b594d5dc88fe8
Cody <cody@dreamersofday.co>
2018-10-28 23:29:07 -0500

    clean up home route to use default layout

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,47 +1,42 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import { DefaultLayout } from '../../UI/Layouts';
+import { Heading } from '../../UI/elements';
 import { isAuthenticated } from '../../../reducers/auth';
 
 class Home extends Component {
   renderLinks() {
     if (this.props.isAuthenticated) {
       return (
-        <Fragment>
-          <div className="flex flex-col w-3/4 mx-auto">
-            <Link className="btn btn--white btn--lg mb-6" to="/new-game">New Game</Link>
-            <Link className="btn btn--white btn--lg" to="/games">My Games</Link>
-          </div>
-          <div className="mt-auto">
-            <Link className="btn btn--white mt-auto" to="/signout">Sign Out</Link>
-          </div>
-        </Fragment>
-      )
-    } else {
-      return (
-        <div className="flex flex-col w-3/4 mx-auto">
-          <Link className="btn btn--white btn--lg mb-6" to="/login">Login</Link>
-          <Link className="btn btn--white btn--lg" to="/signup">Sign Up</Link>
+        <div className="text-center">
+            <Link className="block btn btn--white btn--lg mb-6" to="/new-game">New Game</Link>
+            <Link className="block btn btn--white btn--lg mb-8" to="/games">My Games</Link>
+            <Link className="btn btn--white mx-auto" to="/signout">Sign Out</Link>
         </div>
-      )
+      );
     }
+    return (
+      <div className="flex flex-col w-3/4 mx-auto">
+        <Link className="btn btn--white btn--lg mb-6" to="/login">Login</Link>
+        <Link className="btn btn--white btn--lg" to="/signup">Sign Up</Link>
+      </div>
+    );
   }
 
   render() {
     return (
-      <div className="py-8 h-screen flex flex-col text-center container">
-        <h1 className="mb-8 text-white">Bowling Scores</h1>
+      <DefaultLayout>
+        <Heading className="mb-8">Bowling Scores</Heading>
         {this.renderLinks()}
-      </div>
+      </DefaultLayout>
     );
   }
 }
 
-const mapStateToProps = state => {
-  return {
-    isAuthenticated: isAuthenticated(state),
-  };
-};
+const mapStateToProps = state => ({
+  isAuthenticated: isAuthenticated(state),
+});
 
 export default connect(mapStateToProps)(Home);
